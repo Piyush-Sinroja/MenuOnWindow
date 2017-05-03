ade37a83e66dfd69ddbe5124454380ebb323b059
Piyush Sinroja <piyuengi007@gmail.com>
2017-05-03 16:29:40 +0530

    changes obj

--- MenuOnWindow/MenuOnWindow/HelperClass/MenuOnWindow.swift
+++ MenuOnWindow/MenuOnWindow/HelperClass/MenuOnWindow.swift
@@ -15,27 +15,27 @@ var screenHeight: CGFloat = UIScreen.main.bounds.size.height
 class MenuOnWindow: NSObject {
     
     var windowApp: UIWindow?
-    var btnMenu = UIButton()
-    static var xibView : ViewMenu!
+    static var btnMenu = UIButton()
+    static var xibView : ViewMenu?
     static var navController = UINavigationController()
     
     //MARK:- Btn Load On Window
     func loadNewView(window: UIWindow, navigation: UINavigationController) {
         windowApp = window
         MenuOnWindow.navController = navigation
-        btnMenu = UIButton(type: .custom)
-        btnMenu.setTitle("Menu", for: .normal)
-        btnMenu.titleLabel?.textAlignment = .center
-        btnMenu.titleLabel!.font = UIFont.systemFont(ofSize: 15)
-        btnMenu.setTitleColor(UIColor.white, for: .normal)
-        btnMenu.backgroundColor = UIColor.black
-        btnMenu.frame = CGRect(x: 10, y: 20, width: 45, height: 45)
-        btnMenu.layer.cornerRadius = btnMenu.frame.size.width/2
-        btnMenu.layer.masksToBounds = true
-        btnMenu.addTarget(self, action: #selector(btnMenuAction(sender:)), for: .touchUpInside)
+        MenuOnWindow.btnMenu = UIButton(type: .custom)
+        MenuOnWindow.btnMenu.setTitle("Menu", for: .normal)
+        MenuOnWindow.btnMenu.titleLabel?.textAlignment = .center
+        MenuOnWindow.btnMenu.titleLabel!.font = UIFont.systemFont(ofSize: 15)
+        MenuOnWindow.btnMenu.setTitleColor(UIColor.white, for: .normal)
+        MenuOnWindow.btnMenu.backgroundColor = UIColor.black
+        MenuOnWindow.btnMenu.frame = CGRect(x: 10, y: 20, width: 45, height: 45)
+        MenuOnWindow.btnMenu.layer.cornerRadius = MenuOnWindow.btnMenu.frame.size.width/2
+        MenuOnWindow.btnMenu.layer.masksToBounds = true
+        MenuOnWindow.btnMenu.addTarget(self, action: #selector(btnMenuAction(sender:)), for: .touchUpInside)
         let panner = UIPanGestureRecognizer(target: self, action: #selector(self.panDidFire(panner:)))
-        btnMenu.addGestureRecognizer(panner)
-        window.addSubview(btnMenu)
+        MenuOnWindow.btnMenu.addGestureRecognizer(panner)
+        window.addSubview(MenuOnWindow.btnMenu)
         window.makeKeyAndVisible()
     }
     
@@ -43,24 +43,24 @@ class MenuOnWindow: NSObject {
     func panDidFire(panner: UIPanGestureRecognizer) {
         let offset = panner.translation(in: windowApp)
         panner.setTranslation(CGPoint.zero, in: windowApp)
-        var center = btnMenu.center
+        var center = MenuOnWindow.btnMenu.center
         center.x += offset.x
         center.y += offset.y
-        btnMenu.center = center
+        MenuOnWindow.btnMenu.center = center
         if panner.state == .ended || panner.state == .cancelled {
             UIView.animate(withDuration: 0.3) {
-                if self.btnMenu.frame.origin.x < 0 {
-                    self.btnMenu.frame = CGRect(x: 10, y: self.btnMenu.frame.origin.y, width: self.btnMenu.frame.size.width, height: self.btnMenu.frame.size.height)
+                if MenuOnWindow.btnMenu.frame.origin.x < 0 {
+                   MenuOnWindow.btnMenu.frame = CGRect(x: 10, y: MenuOnWindow.btnMenu.frame.origin.y, width: MenuOnWindow.btnMenu.frame.size.width, height: MenuOnWindow.btnMenu.frame.size.height)
                 }
-                else if (self.btnMenu.frame.origin.x + self.btnMenu.frame.size.width) > screenWidth {
-                    self.btnMenu.frame = CGRect(x:screenWidth - self.btnMenu.frame.size.width - 10, y: self.btnMenu.frame.origin.y, width: self.btnMenu.frame.size.width, height: self.btnMenu.frame.size.height)
+                else if (MenuOnWindow.btnMenu.frame.origin.x + MenuOnWindow.btnMenu.frame.size.width) > screenWidth {
+                    MenuOnWindow.btnMenu.frame = CGRect(x:screenWidth - MenuOnWindow.btnMenu.frame.size.width - 10, y: MenuOnWindow.btnMenu.frame.origin.y, width: MenuOnWindow.btnMenu.frame.size.width, height: MenuOnWindow.btnMenu.frame.size.height)
                 }
-                if self.btnMenu.frame.origin.y < 0 {
-                    self.btnMenu.frame = CGRect(x: self.btnMenu.frame.origin.x, y: 20, width: self.btnMenu.frame.size.width, height: self.btnMenu.frame.size.height)
+                if MenuOnWindow.btnMenu.frame.origin.y < 0 {
+                    MenuOnWindow.btnMenu.frame = CGRect(x: MenuOnWindow.btnMenu.frame.origin.x, y: 20, width: MenuOnWindow.btnMenu.frame.size.width, height: MenuOnWindow.btnMenu.frame.size.height)
                     
                 }
-                else if (self.btnMenu.frame.origin.y + self.btnMenu.frame.size.height) > screenHeight {
-                    self.btnMenu.frame = CGRect(x:self.btnMenu.frame.origin.x, y: screenHeight - self.btnMenu.frame.size.height - 10, width: self.btnMenu.frame.size.width, height: self.btnMenu.frame.size.height)
+                else if (MenuOnWindow.btnMenu.frame.origin.y + MenuOnWindow.btnMenu.frame.size.height) > screenHeight {
+                    MenuOnWindow.btnMenu.frame = CGRect(x:MenuOnWindow.btnMenu.frame.origin.x, y: screenHeight - MenuOnWindow.btnMenu.frame.size.height - 10, width: MenuOnWindow.btnMenu.frame.size.width, height: MenuOnWindow.btnMenu.frame.size.height)
                 }
             }
         }
@@ -77,47 +77,48 @@ class MenuOnWindow: NSObject {
     //MARK:- TouchView Method
     func touchViewRemove() {
         if MenuOnWindow.xibView != nil {
-            MenuOnWindow.xibView.removeFromSuperview()
+            MenuOnWindow.xibView?.removeFromSuperview()
             MenuOnWindow.xibView = nil
+            MenuOnWindow.btnMenu.setTitle("Open", for:.normal)
         }
     }
     
     func touchViewAddOrRemove()  {
         if MenuOnWindow.xibView != nil {
-            MenuOnWindow.xibView.removeFromSuperview()
+            MenuOnWindow.xibView?.removeFromSuperview()
             MenuOnWindow.xibView = nil
-            btnMenu.setTitle("Open", for:.normal)
+            MenuOnWindow.btnMenu.setTitle("Open", for:.normal)
         }
         else {
-            btnMenu.setTitle("Close", for:.normal)
-            MenuOnWindow.xibView = Bundle.main.loadNibNamed("ViewMenu", owner: self, options: nil)?[0] as! ViewMenu
+            MenuOnWindow.btnMenu.setTitle("Close", for:.normal)
+            MenuOnWindow.xibView = Bundle.main.loadNibNamed("ViewMenu", owner: self, options: nil)?[0] as? ViewMenu
             
-            MenuOnWindow.xibView.dropShadow()
-            MenuOnWindow.xibView.viewTbl.layer.cornerRadius = 5.0
-            MenuOnWindow.xibView.viewTbl.layer.masksToBounds = true
+            MenuOnWindow.xibView?.dropShadow()
+            MenuOnWindow.xibView?.viewTbl.layer.cornerRadius = 5.0
+            MenuOnWindow.xibView?.viewTbl.layer.masksToBounds = true
             
-            MenuOnWindow.xibView.registerMethod()
-            let btnCenter =  btnMenu.center
-            MenuOnWindow.xibView.layer.cornerRadius = 5.0
+            MenuOnWindow.xibView?.registerMethod()
+            let btnCenter =  MenuOnWindow.btnMenu.center
+            MenuOnWindow.xibView?.layer.cornerRadius = 5.0
             if btnCenter.x > (screenWidth/2)  {
                 if btnCenter.y > (screenHeight/2) {
-                    MenuOnWindow.xibView.frame = CGRect(x: btnMenu.frame.origin.x - viewWidthHeight, y: btnMenu.frame.origin.y - viewWidthHeight , width: viewWidthHeight, height: viewWidthHeight)
+                    MenuOnWindow.xibView?.frame = CGRect(x: MenuOnWindow.btnMenu.frame.origin.x - viewWidthHeight, y: MenuOnWindow.btnMenu.frame.origin.y - viewWidthHeight , width: viewWidthHeight, height: viewWidthHeight)
                 }
                 else {
-                    MenuOnWindow.xibView.frame = CGRect(x: btnMenu.frame.origin.x - viewWidthHeight, y: btnMenu.frame.origin.y +  btnMenu.frame.height, width: viewWidthHeight, height: viewWidthHeight)
+                    MenuOnWindow.xibView?.frame = CGRect(x: MenuOnWindow.btnMenu.frame.origin.x - viewWidthHeight, y: MenuOnWindow.btnMenu.frame.origin.y +  MenuOnWindow.btnMenu.frame.height, width: viewWidthHeight, height: viewWidthHeight)
                 }
             }
             else {
                 if btnCenter.y > (screenHeight/2) {
-                    MenuOnWindow.xibView.frame = CGRect(x: btnMenu.frame.origin.x + btnMenu.frame.width, y: btnMenu.frame.origin.y - viewWidthHeight, width: viewWidthHeight, height: viewWidthHeight)
+                    MenuOnWindow.xibView?.frame = CGRect(x: MenuOnWindow.btnMenu.frame.origin.x + MenuOnWindow.btnMenu.frame.width, y: MenuOnWindow.btnMenu.frame.origin.y - viewWidthHeight, width: viewWidthHeight, height: viewWidthHeight)
                 }
                 else {
-                    MenuOnWindow.xibView.frame = CGRect(x: btnMenu.frame.origin.x + btnMenu.frame.width, y: btnMenu.frame.origin.y +  btnMenu.frame.height, width: viewWidthHeight, height: viewWidthHeight)
+                    MenuOnWindow.xibView?.frame = CGRect(x: MenuOnWindow.btnMenu.frame.origin.x + MenuOnWindow.btnMenu.frame.width, y: MenuOnWindow.btnMenu.frame.origin.y +  MenuOnWindow.btnMenu.frame.height, width: viewWidthHeight, height: viewWidthHeight)
                 }
             }
-            MenuOnWindow.xibView.backgroundColor = UIColor.green
-            windowApp?.addSubview(MenuOnWindow.xibView)
-            windowApp?.bringSubview(toFront: btnMenu)
+            MenuOnWindow.xibView?.backgroundColor = UIColor.green
+            windowApp?.addSubview(MenuOnWindow.xibView!)
+            windowApp?.bringSubview(toFront: MenuOnWindow.btnMenu)
         }
     }
     
